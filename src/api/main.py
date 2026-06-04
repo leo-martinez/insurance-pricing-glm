308880e2d48c1b22852c5ca43a801057261d57d1
Leonardo Martinez <leonardobarreirosmartinez@gmail.com>
2026-06-04 19:32:02 -0300

    add CORS middleware

--- src/api/main.py
+++ src/api/main.py
@@ -55,6 +55,12 @@ app = FastAPI(
     version="1.0.0",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
